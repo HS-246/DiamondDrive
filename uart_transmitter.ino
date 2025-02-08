ed6465b604412b8312ceaeded02ccd1cd4886f74
ishanurg <127853515+ishanurg@users.noreply.github.com>
2025-02-08 18:05:35 +0530

    Delete uart_transmitter.ino

--- uart_transmitter.ino
+++
@@ -1,8 +0,0 @@
-void setup() {
-  Serial.begin(9600); // Default UART on TX (GPIO1) and RX (GPIO3)
-}
-
-void loop() {
-  Serial.println("HELLO WORLD"); // Send data over UART
-  delay(1000); // Send every second
-}
